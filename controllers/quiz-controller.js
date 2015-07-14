b9d447a9f00efe78e0e1ab3f938ad9c790472f4a
jralfer <alvferjs@gmail.com>
2015-07-14 15:15:22 +0200

    Lista de preguntas

--- controllers/quiz-controller.js
+++ controllers/quiz-controller.js
@@ -2,23 +2,36 @@
 
 var models = require('../models/models.js');
 
-// GET quizes/question
-exports.question = function(req, res) {
-    models.Quiz.findAll().success(function(quiz) {
-        res.render('quizes/question', {pregunta: quiz[0].pregunta});
+
+
+// GET /quizes
+exports.index = function(req, res) {
+    models.Quiz.findAll().then(
+        function(quizes) {
+            res.render('quizes/index', {quizes: quizes});
+        }
+    )
+};
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+    models.Quiz.find(req.params.quizId).then(function(quiz) {
+        res.render('quizes/show', {quiz: quiz});
     })
 };
 
-
-// GET quizes/answer
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-    models.Quiz.findAll().success(function(quiz) {
-        if(req.query.respuesta === quiz[0].respuesta) {
-            res.render('quizes/question', {pregunta: "correcto"});
+    models.Quiz.find(req.params.quizId).then(function(quiz) {
+    var resultado = 'Incorrecto';
+    if (req.query.respuesta === quiz.respuesta) {
+        resultado = 'Correcto';
+    }
+    res.render(
+        'quizes/answer',
+        { quiz: quiz,
+            respuesta: resultado
         }
-        else{
-            res.render('quizes/question', {pregunta: "incorrecto"});
-        }
-    })
-};
+    )}
+    )};
 
--- models/models.js
+++ models/models.js
@@ -3,14 +3,14 @@ var path = require('path');
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite   DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name  = (url[6]||null);
-var user     = (url[2]||null);
-var pwd      = (url[3]||null);
-var protocol = (url[1]||null);
-var dialect  = (url[1]||null);
-var port     = (url[5]||null);
-var host     = (url[4]||null);
-var storage  = process.env.DATABASE_STORAGE;
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
 
 
 // Cargar Modelo ORM
@@ -19,11 +19,12 @@ var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize(DB_name, user, pwd,
-    { dialect:  protocol,
+    {
+        dialect: protocol,
         protocol: protocol,
-        port:     port,
-        host:     host,
-        storage:  storage,  // solo SQLite (.env)
+        port: port,
+        host: host,
+        storage: storage,  // solo SQLite (.env)
         omitNull: true      // solo Postgres
     }
 );
@@ -38,21 +39,21 @@ exports.Quiz = Quiz;
 
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function () {
+sequelize.sync().then(function () {
     //ejecuta el manejador una vez creada la tabla
 
-    Quiz.count().success(function (count) {
+    Quiz.count().then(function (count) {
         if (count === 0) {   // la tabla se inicializa solo si está vacía
+
+
             Quiz.bulkCreate(
                 [{pregunta: '¿Cuál es la capital de Italia?', respuesta: 'Roma'},
                     {pregunta: '¿Cuál es la capital de Portugal?', respuesta: 'Lisboa'},
                     {pregunta: '¿Quién descubrió América?', respuesta: 'Cristóbal Colón'}
-                ]
-            ).then(function () {
+                ]).then(function () {
                     console.log('Base de datos (tabla quiz) inicializada')
                 });
         }
-        ;
     });
 });
 
